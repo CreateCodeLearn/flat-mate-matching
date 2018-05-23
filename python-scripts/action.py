a5c7ae3c2b13e03008085c6a6148d39e5232101c
Mieter <mieter@Mediahelds-MacBook-Pro.local>
2018-05-23 12:53:55 +0200

    fake data best fit

--- python-scripts/action.py
+++ python-scripts/action.py
@@ -3,9 +3,14 @@
 import cgi
 #from Algorithm import myAlg
 
+
 #get the output of the form.
 form = cgi.FieldStorage()
 
+import pickle
+#import pickle to be able to read the binariy file
+data = pickle.load(open("./python-scripts/fake_dict_data.p","rb"))
+#get output from the file and name it as "data"
 
 #get an input filed from the form callled 'name'
 #and assign it's value to a local variable called v_name
@@ -13,6 +18,24 @@ v_gender = form.getvalue('Gender')
 v_area = form.getvalue('Area')
 v_age = form.getvalue('myAge')
 v_budget = form.getvalue('myBudget')
+
+
+#extracting data from the tuple
+flatsupplier = data[0]
+
+flatorder = data[1]
+
+print (flatorder)
+
+print (flatorder[0])
+#best fit, as the 1st position on flatorder
+
+bestfit = flatsupplier.get(flatorder[0])
+#extract from dict file the key for flatorder 0
+print (bestfit)
+
+Location = bestfit.get("Location")
+print (Location)
 
 #send an html response.
 print ("""
@@ -28,3 +51,5 @@ Budget: %s
 </html>
 """ % (v_gender,v_area,v_age,v_budget
 ))
+#print the data
+#print (data)
